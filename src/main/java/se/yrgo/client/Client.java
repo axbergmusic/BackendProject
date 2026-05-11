032fed1efcd028f252d0cac25c80fc7c9a927d71
Alienoro <sebban952@gmail.com>
2026-05-11 19:39:00 +0200

    adjusted prices to match real life

--- src/main/java/se/yrgo/client/Client.java
+++ src/main/java/se/yrgo/client/Client.java
@@ -197,7 +197,7 @@ public class Client {
         skin1.setType(Type.RIFLE);
         skin1.setRarity(Rarity.CLASSIFIED);
         skin1.setCondition(Condition.FIELDTESTED);
-        skin1.setPrice(15.99);
+        skin1.setPrice(35.99);
         skinService.create(skin1);
 
         Skin skin2 = new Skin();
@@ -205,7 +205,7 @@ public class Client {
         skin2.setType(Type.RIFLE);
         skin2.setRarity(Rarity.COVERT);
         skin2.setCondition(Condition.FACTORYNEW);
-        skin2.setPrice(2499.99);
+        skin2.setPrice(13322.45);
         skinService.create(skin2);
 
         Skin skin3 = new Skin();
@@ -213,7 +213,7 @@ public class Client {
         skin3.setType(Type.RIFLE);
         skin3.setRarity(Rarity.CONTRABAND);
         skin3.setCondition(Condition.MINIMALWEAR);
-        skin3.setPrice(1899.50);
+        skin3.setPrice(6163.51);
         skinService.create(skin3);
 
         Skin skin4 = new Skin();
@@ -221,7 +221,7 @@ public class Client {
         skin4.setType(Type.SMG);
         skin4.setRarity(Rarity.MILSPEC);
         skin4.setCondition(Condition.FIELDTESTED);
-        skin4.setPrice(2.35);
+        skin4.setPrice(1.35);
         skinService.create(skin4);
 
         Skin skin5 = new Skin();
@@ -229,7 +229,7 @@ public class Client {
         skin5.setType(Type.PISTOL);
         skin5.setRarity(Rarity.COVERT);
         skin5.setCondition(Condition.FIELDTESTED);
-        skin5.setPrice(89.99);
+        skin5.setPrice(76.10);
         skinService.create(skin5);
 
         Skin skin6 = new Skin();
@@ -237,7 +237,7 @@ public class Client {
         skin6.setType(Type.PISTOL);
         skin6.setRarity(Rarity.RESTRICTED);
         skin6.setCondition(Condition.FACTORYNEW);
-        skin6.setPrice(1200.00);
+        skin6.setPrice(2102.83);
         skinService.create(skin6);
 
         Skin skin7 = new Skin();
@@ -245,7 +245,7 @@ public class Client {
         skin7.setType(Type.MELEE);
         skin7.setRarity(Rarity.GOLD);
         skin7.setCondition(Condition.MINIMALWEAR);
-        skin7.setPrice(2352.00);
+        skin7.setPrice(2592.83);
         skinService.create(skin7);
 
         Skin skin8 = new Skin();
@@ -253,7 +253,7 @@ public class Client {
         skin8.setType(Type.SMG);
         skin8.setRarity(Rarity.COVERT);
         skin8.setCondition(Condition.BATTLESCARRED);
-        skin8.setPrice(32.40);
+        skin8.setPrice(165.80);
         skinService.create(skin8);
 
         Skin skin9 = new Skin();
@@ -261,7 +261,7 @@ public class Client {
         skin9.setType(Type.SMG);
         skin9.setRarity(Rarity.CLASSIFIED);
         skin9.setCondition(Condition.WELLWORN);
-        skin9.setPrice(8.75);
+        skin9.setPrice(5.75);
         skinService.create(skin9);
 
         Skin skin10 = new Skin();
@@ -269,7 +269,7 @@ public class Client {
         skin10.setType(Type.RIFLE);
         skin10.setRarity(Rarity.COVERT);
         skin10.setCondition(Condition.MINIMALWEAR);
-        skin10.setPrice(21.35);
+        skin10.setPrice(39.92);
         skinService.create(skin10);
 
         Skin skin11 = new Skin();
@@ -285,7 +285,7 @@ public class Client {
         skin12.setType(Type.PISTOL);
         skin12.setRarity(Rarity.RESTRICTED);
         skin12.setCondition(Condition.FACTORYNEW);
-        skin12.setPrice(450.75);
+        skin12.setPrice(837.39);
         skinService.create(skin12);
 
         Skin skin13 = new Skin();
@@ -293,7 +293,7 @@ public class Client {
         skin13.setType(Type.PISTOL);
         skin13.setRarity(Rarity.MILSPEC);
         skin13.setCondition(Condition.WELLWORN);
-        skin13.setPrice(1.80);
+        skin13.setPrice(0.58);
         skinService.create(skin13);
 
         Skin skin14 = new Skin();
@@ -301,7 +301,7 @@ public class Client {
         skin14.setType(Type.GLOVES);
         skin14.setRarity(Rarity.GOLD);
         skin14.setCondition(Condition.FACTORYNEW);
-        skin14.setPrice(7600.92);
+        skin14.setPrice(8449.92);
         skinService.create(skin14);
 
         Skin skin15 = new Skin();
@@ -309,7 +309,7 @@ public class Client {
         skin15.setType(Type.RIFLE);
         skin15.setRarity(Rarity.CLASSIFIED);
         skin15.setCondition(Condition.MINIMALWEAR);
-        skin15.setPrice(28.99);
+        skin15.setPrice(159.40);
         skinService.create(skin15);
 
         Skin skin16 = new Skin();
@@ -317,7 +317,7 @@ public class Client {
         skin16.setType(Type.RIFLE);
         skin16.setRarity(Rarity.COVERT);
         skin16.setCondition(Condition.FIELDTESTED);
-        skin16.setPrice(45.00);
+        skin16.setPrice(202.80);
         skinService.create(skin16);
 
         Skin skin17 = new Skin();
@@ -325,7 +325,7 @@ public class Client {
         skin17.setType(Type.PISTOL);
         skin17.setRarity(Rarity.MILSPEC);
         skin17.setCondition(Condition.FACTORYNEW);
-        skin17.setPrice(3.10);
+        skin17.setPrice(2.48);
         skinService.create(skin17);
 
         Skin skin18 = new Skin();
@@ -333,7 +333,7 @@ public class Client {
         skin18.setType(Type.RIFLE);
         skin18.setRarity(Rarity.CONTRABAND);
         skin18.setCondition(Condition.FIELDTESTED);
-        skin18.setPrice(2600.00);
+        skin18.setPrice(619712.17);
         skinService.create(skin18);
 
         Skin skin19 = new Skin();
@@ -341,7 +341,7 @@ public class Client {
         skin19.setType(Type.PISTOL);
         skin19.setRarity(Rarity.RESTRICTED);
         skin19.setCondition(Condition.MINIMALWEAR);
-        skin19.setPrice(12.75);
+        skin19.setPrice(4.08);
         skinService.create(skin19);
 
         Skin skin20 = new Skin();
@@ -349,7 +349,7 @@ public class Client {
         skin20.setType(Type.MELEE);
         skin20.setRarity(Rarity.GOLD);
         skin20.setCondition(Condition.WELLWORN);
-        skin20.setPrice(950.00);
+        skin20.setPrice(593.77);
         skinService.create(skin20);
     }
 }
