fc56ab4e72e3411db6057cb96fa353cf95f60f9f
kallehallback <kalle.hallback@gmail.com>
2026-05-11 10:17:37 +0200

    aop logging

---
+++ src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,30 @@
+package se.yrgo.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceTimingAdvice {
+
+    @Around("execution(* se.yrgo..*(..))")
+    public Object performTimingMeasurement(ProceedingJoinPoint method) throws Throwable {
+
+        // before
+        long startTime = System.nanoTime();
+
+        try {
+            // proceed to target
+            Object value = method.proceed();
+            return value;
+
+        } finally {
+            // after
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            System.out.println("The method " + method.getSignature().getName()
+                    + " took " + timeTaken / 1000000 + " ms");
+        }
+    }
+}
